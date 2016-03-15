8668fd1ab814490713278567792feb5d9d7add61
David Solis <david@soliskit.com>
2016-03-14 20:59:51 -0700

    fixed company tests

--- spec/views/companies/index.html.erb_spec.rb
+++ spec/views/companies/index.html.erb_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe "companies/index", type: :view do
   before(:each) do
     assign(:companies, [
       Company.create!(
-        :name => "Name",
-        :entity_number => "Entity Number",
+        :name => "Small Business 1",
+        :entity_number => 1,
         :address_1 => "Address 1",
         :address_2 => "Address 2",
         :city => "City",
         :state => nil,
         :zip => "Zip",
         :business_structure => "Business Structure",
-        :year_established => 1,
-        :number_of_employees => 2,
+        :year_established => 2,
+        :number_of_employees => 3,
         :last_year_sales => "9.99",
-        :counties => "MyText",
+        :counties => "LA County",
         :general_liability_insurance_amount => "9.99",
         :contract_dollars_level_interest_min => "9.99",
         :contract_dollars_level_interest_max => "9.99",
@@ -25,23 +25,23 @@ RSpec.describe "companies/index", type: :view do
         :type_of_construction => "Type Of Construction"
       ),
       Company.create!(
-        :name => "Name",
-        :entity_number => "Entity Number",
+        :name => "Small Business 2",
+        :entity_number => 4,
         :address_1 => "Address 1",
         :address_2 => "Address 2",
         :city => "City",
         :state => nil,
         :zip => "Zip",
         :business_structure => "Business Structure",
-        :year_established => 1,
-        :number_of_employees => 2,
+        :year_established => 5,
+        :number_of_employees => 6,
         :last_year_sales => "9.99",
-        :counties => "MyText",
+        :counties => "Orange County",
         :general_liability_insurance_amount => "9.99",
         :contract_dollars_level_interest_min => "9.99",
         :contract_dollars_level_interest_max => "9.99",
         :nature_of_business => "MyText",
-        :is_union_contractor => false,
+        :is_union_contractor => true,
         :bonding_capacity => "Bonding Capacity",
         :type_of_construction => "Type Of Construction"
       )
@@ -50,23 +50,26 @@ RSpec.describe "companies/index", type: :view do
 
   it "renders a list of companies" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Entity Number".to_s, :count => 2
+    assert_select "tr>td", :text => "Small Business 1".to_s, :count => 1
+    assert_select "tr>td", :text => 1.to_s, :count => 1
+    assert_select "tr>td", :text => "Small Business 2".to_s, :count => 1
+    assert_select "tr>td", :text => 2.to_s, :count => 1
     assert_select "tr>td", :text => "Address 1".to_s, :count => 2
     assert_select "tr>td", :text => "Address 2".to_s, :count => 2
     assert_select "tr>td", :text => "City".to_s, :count => 2
     assert_select "tr>td", :text => nil.to_s, :count => 2
     assert_select "tr>td", :text => "Zip".to_s, :count => 2
     assert_select "tr>td", :text => "Business Structure".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td", :text => 3.to_s, :count => 1
+    assert_select "tr>td", :text => 4.to_s, :count => 1
+    assert_select "tr>td", :text => 5.to_s, :count => 1
+    assert_select "tr>td", :text => 6.to_s, :count => 1
+    assert_select "tr>td", :text => "9.99".to_s, :count => 8
+    assert_select "tr>td", :text => "LA County".to_s, :count => 1
+    assert_select "tr>td", :text => "Orange County".to_s, :count => 1
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", :text => false.to_s, :count => 1
+    assert_select "tr>td", :text => true.to_s, :count => 1
     assert_select "tr>td", :text => "Bonding Capacity".to_s, :count => 2
     assert_select "tr>td", :text => "Type Of Construction".to_s, :count => 2
   end
--- spec/views/companies/show.html.erb_spec.rb
+++ spec/views/companies/show.html.erb_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe "companies/show", type: :view do
   before(:each) do
     @company = assign(:company, Company.create!(
-      :name => "Name",
-      :entity_number => "Entity Number",
+      :name => "Small Business 1",
+      :entity_number => 1,
       :address_1 => "Address 1",
       :address_2 => "Address 2",
       :city => "City",
       :state => nil,
       :zip => "Zip",
       :business_structure => "Business Structure",
-      :year_established => 1,
-      :number_of_employees => 2,
+      :year_established => 2,
+      :number_of_employees => 3,
       :last_year_sales => "9.99",
-      :counties => "MyText",
+      :counties => "LA County",
       :general_liability_insurance_amount => "9.99",
       :contract_dollars_level_interest_min => "9.99",
       :contract_dollars_level_interest_max => "9.99",
@@ -27,18 +27,18 @@ RSpec.describe "companies/show", type: :view do
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Entity Number/)
+    expect(rendered).to match(/Small Business 1/)
+    expect(rendered).to match(/1/)
     expect(rendered).to match(/Address 1/)
     expect(rendered).to match(/Address 2/)
     expect(rendered).to match(/City/)
     expect(rendered).to match(//)
     expect(rendered).to match(/Zip/)
     expect(rendered).to match(/Business Structure/)
-    expect(rendered).to match(/1/)
     expect(rendered).to match(/2/)
+    expect(rendered).to match(/3/)
     expect(rendered).to match(/9.99/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/LA County/)
     expect(rendered).to match(/9.99/)
     expect(rendered).to match(/9.99/)
     expect(rendered).to match(/9.99/)
